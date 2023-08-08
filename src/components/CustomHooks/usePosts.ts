c6b8716f3746b6acad8f6c226e4bab1d9f571286
lucasmuri777 <62767967+lucasmuri777@users.noreply.github.com>
2023-08-08 15:42:32 -0300

    Add localStoarage Save

--- src/components/CustomHooks/usePosts.ts
+++ src/components/CustomHooks/usePosts.ts
@@ -1,12 +1,20 @@
 'use client'
 import { PostActions, postReducer } from "@/reducers/postReducer"
 import { Post } from "@/types/Post"
-import { useState, Dispatch, useReducer } from "react"
+import { useState, Dispatch, useReducer, useEffect } from "react"
 
 
 
 export const usePosts = () =>{
-    const [posts, dispatch] = useReducer(postReducer, []);
+    const [posts, dispatch] = useReducer(
+            postReducer, 
+            JSON.parse(localStorage.getItem('posts') || '[]')
+        );
+
+        useEffect(()=>{
+            localStorage.setItem('posts', JSON.stringify(posts))
+        }, [posts]);
+
     const addPost = (title: string, body: string) => {
         dispatch({
             type: 'add',
